553fad29c87e04224577474cbce8664d9202d670
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-12-12 18:01:30 +0000

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -23,7 +23,7 @@ void send_command_to_server(Request *msg) {
     close(fd);
 }
 
-// Función para manejar la señal SIGINT
+// Función para manejar la señal SIGINT (CTRL+C del cliente)
 void handle_sigint(int sig) {
     printf("\nSe recibió la señal SIGINT. Limpiando recursos...\n");
     msg.command_type = 9;
@@ -32,14 +32,14 @@ void handle_sigint(int sig) {
     exit(0);
 }
 
-// Función para manejar la señal SIGTERM
+// Función para manejar la señal SIGTERM (close, remove y CTRL+C del manager)
 void handle_sigterm(int sig) {
     printf("\nSe recibió la señal SIGTERM. Cerrando el cliente...\n");
     unlink(msg.client_pipe);
     exit(0);
 }
 
-// Configurar los manejadores de señales
+// Configura manejadores personalizados para las señales SIGINT y SIGTERM
 void setup_signal_handlers() {
     struct sigaction sa;
 
@@ -76,16 +76,16 @@ void handle_user_input() {
         msg.topic[0] = '\0';
         send_command_to_server(&msg);
 
-    } else if (strncmp(input, "unsubscribe ", 12) == 0) {
-        msg.command_type = 4;
-        strncpy(msg.topic, input + 12, sizeof(msg.topic));
-        send_command_to_server(&msg);
-
     } else if (strcmp(input, "exit") == 0) {
         msg.command_type = 3;
         printf("Cliente: Saliendo...\n");
         send_command_to_server(&msg);
         exit(0);
+
+    } else if (strncmp(input, "unsubscribe ", 12) == 0) {
+        msg.command_type = 4;
+        strncpy(msg.topic, input + 12, sizeof(msg.topic));
+        send_command_to_server(&msg);
 
     } else if (strncmp(input, "msg ", 4) == 0) {
         char topic[20];
@@ -114,19 +114,20 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Llamada a los manejadores de señales
+    // Llamada a la función que configura los manejadores de señales
     setup_signal_handlers();
 
     // Asignamos el nombre de usuario y PID al mensaje
-    strncpy(msg.username, argv[1], sizeof(msg.username) - 1); // obtenemos el nombre del usuario
-    msg.pid = getpid(); // obtenemos el pid del usuario
+    strncpy(msg.username, argv[1], sizeof(msg.username) - 1);
+    msg.pid = getpid();
     msg.username[sizeof(msg.username) - 1] = '\0'; // nos aseguramos que el último índice del array es la finalización
 
     // Usamos el PID para crear el nombre del pipe
     snprintf(msg.client_pipe, sizeof(msg.client_pipe), "client_pipe_%d", msg.pid);
     mkfifo(msg.client_pipe, 0600);
 
-    msg.command_type = 0; // comando para inicio de sesión
+    // Comando para inicio de sesión (0)
+    msg.command_type = 0; 
     send_command_to_server(&msg);
     printf("Mensaje enviado al servidor: %s\n", msg.username);
 
@@ -141,14 +142,15 @@ int main(int argc, char *argv[]) {
     // Bucle infinito para leer y escribir comandos
     while (1) {
         fd_set read_fds;
-        FD_ZERO(&read_fds); // Limpia el conjunto de descriptores de archivo
-        FD_SET(0, &read_fds); // Añade la entrada estándar al conjunto.
-        FD_SET(client_fd, &read_fds); // Añade el descriptor del pipe del cliente al conjunto.
+        FD_ZERO(&read_fds); // limpia el conjunto de descriptores de archivo
+        FD_SET(0, &read_fds); // añade la entrada estándar al conjunto.
+        FD_SET(client_fd, &read_fds); // añade el descriptor del pipe del cliente al conjunto.
 
-        //int max_fd = (STDIN_FILENO > client_fd) ? STDIN_FILENO : client_fd; // Calcula el descriptor de archivo más alto.
-        int activity = select(client_fd + 1, &read_fds, NULL, NULL, NULL); // Espera actividad en los descriptores de archivo especificados.
+        // Espera actividad en los descriptores de archivo especificados.
+        int activity = select(client_fd + 1, &read_fds, NULL, NULL, NULL);
 
-        if (activity == -1) { // Comprueba si ocurrió un error en select
+        // Comprueba si ocurrió un error en select
+        if (activity == -1) {
             perror("Error en select");
             break;
         }
@@ -163,13 +165,10 @@ int main(int argc, char *argv[]) {
             char response[256];
             ssize_t bytes_read = read(client_fd, response, sizeof(response) - 1);
             if (bytes_read > 0) {
-                response[bytes_read] = '\0';
+                response[bytes_read] = '\0'; // la cadena debe acabarse con el caracter nulo
                 printf("Respuesta del servidor: %s\n", response);
             }
         }
     }
-
-    close(client_fd);
-    unlink(msg.client_pipe);
     return 0;
 }
