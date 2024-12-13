6c8c71d2dfa8be68b3a990cccf5867ef2cc26f9d
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2024-12-13 19:03:54 +0000

    Update util.h

--- util.h
+++ util.h
@@ -6,17 +6,14 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/select.h>
-//manager
 #include <pthread.h>
 #include <time.h>
 
 #define SERVER_PIPE "server_pipe"
 #define MAX_TOPICS 20
-#define TOPIC_NAME_LEN 20
+#define TOPIC_NAME_LEN 21 // espacio adicional para el caracter nulo
 #define MAX_SUBSCRIBERS 10
-#define USERNAME_LEN 256
+#define USERNAME_LEN 257 // espacio adicional para el caracter nulo
 #define MAX_USERS 10
 #define MAX_MESSAGES 100
-#define TAM_MSG 300
-//feed
-#define MAX_MSG_LEN 100
+#define TAM_MSG 301 // espacio adicional para el caracter nulo
